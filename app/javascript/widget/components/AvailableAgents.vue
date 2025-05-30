fcfe8752c4f4c6ac74e03d1bc3047468eb9c2b8d
JacquelinneCM <jacquelinne.castro@apli.jobs>
2025-05-30 14:08:01 -0600

    add files

---
+++ app/javascript/widget/components/AvailableAgents.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'AvailableAgents',
+  props: {
+    agents: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="flex space-x-2">
+    <div
+      v-for="agent in agents"
+      :key="agent.id"
+      class="w-8 h-8 rounded-full overflow-hidden border border-gray-300"
+    >
+      <img
+        :src="agent.avatar"
+        :alt="agent.name"
+        class="object-cover w-full h-full"
+      />
+    </div>
+  </div>
+</template>
