b4ee3a19cfe52b25fc5ce57e65078f17b1174ec0
JacquelinneCM <jacquelinne.castro@apli.jobs>
2025-06-04 11:39:08 -0600

    correct files for prettier library

--- app/javascript/dashboard/components-next/NewConversation/components/WhatsappTemplateParser.vue
+++ app/javascript/dashboard/components-next/NewConversation/components/WhatsappTemplateParser.vue
@@ -45,8 +45,7 @@ const processedString = computed(() => {
 });
 
 const processedStringWithVariableHighlight = computed(() => {
-  const variables = templateString.value.match(
-    /{{\s*([a-zA-Z0-9_]+)\s*}}/g
+  const variables = templateString.value.match(/{{\s*([a-zA-Z0-9_]+)\s*}}/g
   ) || [];
 
   return variables.reduce((result, variable) => {
