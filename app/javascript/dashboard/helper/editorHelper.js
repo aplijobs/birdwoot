10f316a408f6540d9e6a523f0a6c4eef5e437fa7
JacquelinneCM <jacquelinne.castro@apli.jobs>
2025-06-06 14:59:01 -0600

    correct files for prettier library

--- app/javascript/dashboard/helper/editorHelper.js
+++ app/javascript/dashboard/helper/editorHelper.js
@@ -24,7 +24,7 @@ export function cleanSignature(signature) {
         const trimmed = line.trim();
         const isAsteriskRule =
           (trimmed.match(/\*/g) || []).length >= 3 && /^\**$/.test(trimmed);
-          const isDashRule = /^-{2,}$/.test(trimmed);
+        const isDashRule = /^-{2,}$/.test(trimmed);
         return !isAsteriskRule && !isDashRule;
       })
       .join('\n');
